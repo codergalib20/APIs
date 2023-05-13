e3ef43ade52078772bbc3b028c07d350bc7319fc
codergalib20 <coderboygalif@gmail.com>
2023-05-13 22:18:09 +0600

    task manager default route

--- task-manager/index.js
+++ task-manager/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 
 
+app.get('/', (req, res) => {
+    res.send('Welcome to Task Manager')
+})
+
 
 app.listen(5000, () => {
     console.log(`Application running on http://localhost:5000`)
