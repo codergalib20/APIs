ed81bc0128fb59275790c40ba318602cd35ee448
codergalib20 <coderboygalif@gmail.com>
2023-05-13 22:30:43 +0600

    task manager task router

--- task-manager/index.js
+++ task-manager/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 
+// middleware
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome to Task Manager')
 })
 
+// routers
+app.use('/api/v1/tasks', require('./routes/tasks/tasks.route')); // task router
 
 app.listen(5000, () => {
     console.log(`Application running on http://localhost:5000`)
