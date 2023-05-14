39e5c0ac94f11b94e056e34369e5fac6a2b3b866
codergalib20 <coderboygalif@gmail.com>
2023-05-14 23:03:14 +0600

    connecting db

--- task-manager/db/connect.js
+++ task-manager/db/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = () => {
-    return mongoose.connect('mongodb://localhost:27017/learningdb');
+    return mongoose.connect('mongodb://localhost:27017/learning');
 }
 
 module.exports = connectDB;
