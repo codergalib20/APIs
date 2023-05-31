16e52b39f0925a34a0cc14b474f05def802d41c2
codergalib20 <coderboygalif@gmail.com>
2023-05-31 22:40:41 +0600

    removed try catch from all controllers

--- task-manager/resources/tasks/task.controller.js
+++ task-manager/resources/tasks/task.controller.js
@@ -2,59 +2,39 @@ const Task = require("./task.model");
 const asyncWrapper = require('../../utils/asyncWrapper');
 
 const getAllTasks = asyncWrapper(async (req, res) => {
-    try {
-        const tasks = await Task.find();
-        res.status(201).json({
-            data: tasks,
-            message: "Tasks loaded"
-        })
-    }
-    catch (err) {
-        res.status(404).json({ err })
-    }
+    const tasks = await Task.find();
+    res.status(201).json({
+        data: tasks,
+        message: "Tasks loaded"
+    })
 });
-const createATask = (req, res) => {
-    try {
-        const data = Task.create(req.body);
-        res.status(201).json({ message: "Task added", data });
-    }
-    catch (err) {
-        res.status(404).json({ message: "Server error", err })
-    }
+const createATask = async (req, res) => {
+    const data = await Task.create(req.body);
+    res.status(201).json({ message: "Task added", data });
 };
 const getATasks = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const task = await Task.findOne({ _id: id });
-        if (!task) {
-            return res.status(404).json({ message: `Task not found of ${id}` });
-        }
-        res.status(201).json({ task })
+    const id = req.params.id;
+    const task = await Task.findOne({ _id: id });
+    if (!task) {
+        return res.status(404).json({ message: `Task not found of ${id}` });
     }
-    catch (err) { res.status(404).json({ message: "Server error", err }) }
+    res.status(201).json({ task })
 };
 const updateATask = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const task = await Task.findOneAndUpdate({ _id: id }, req.body, { new: true });
-        if (!task) {
-            return res.status(404).json({ message: `Task not found of ${id}` });
-        };
-        res.status(201).json({ task });
-    }
-    catch (err) { res.status(404).json({ message: "Server error", err }) }
+    const id = req.params.id;
+    const task = await Task.findOneAndUpdate({ _id: id }, req.body, { new: true });
+    if (!task) {
+        return res.status(404).json({ message: `Task not found of ${id}` });
+    };
 };
 const deleteATask = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const task = await Task.findOneAndDelete({ _id: id });
-        if (!task) {
-            return res.status(404).json({ message: `Task not found of ${id}` });
-        };
-        res.status(201).json({ message: `Task deleted ${id}`, id });
+    const id = req.params.id;
+    const task = await Task.findOneAndDelete({ _id: id });
+    if (!task) {
+        return res.status(404).json({ message: `Task not found of ${id}` });
+    };
+    res.status(201).json({ message: `Task deleted ${id}`, id });
 
-    }
-    catch (err) { res.status(404).json({ message: "Server error", err }) }
 };
 
 
