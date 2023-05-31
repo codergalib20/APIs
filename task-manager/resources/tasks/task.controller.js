9abe95644187ec1ba1921a57b813c76aeb727a1e
codergalib20 <coderboygalif@gmail.com>
2023-05-31 22:29:02 +0600

    task manager update a task and handle error

--- task-manager/resources/tasks/task.controller.js
+++ task-manager/resources/tasks/task.controller.js
@@ -36,6 +36,10 @@ const updateATask = async (req, res) => {
     try {
         const id = req.params.id;
         const task = await Task.findOneAndUpdate({ _id: id }, req.body, { new: true });
+        if (!task) {
+            return res.status(404).json({ message: `Task not found of ${id}` });
+        };
+        res.status(201).json({ task });
     }
     catch (err) { res.status(404).json({ message: "Server error", err }) }
 };
